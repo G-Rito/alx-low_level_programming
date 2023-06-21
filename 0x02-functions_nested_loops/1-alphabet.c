b53295de60a71a4e2a1e417fd7d35cc36321e590
Gina <givenrito@gmail.com>
2023-06-21 13:25:20 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet to print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char alphbt;
+for (alphbt = 'a'; alphbt <= 'z'; alphbt++)
+_putchar(alphbt);
+_putchar('\n');
+}
