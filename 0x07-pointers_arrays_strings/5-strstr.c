b2847dc73ce8f282cc2132e17587841a4b45c921
Gina <givenrito@gmail.com>
2023-07-03 14:42:57 -0700

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *r = needle;
+
+		while (*i == *r && *r != '\0')
+		{
+			i++;
+			r++;
+		}
+
+		if (*r == '\0')
+			return (haystack);
+	}
+	return (0);
+}
